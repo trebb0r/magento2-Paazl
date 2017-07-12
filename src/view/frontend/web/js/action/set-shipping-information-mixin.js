4c6f6d427eacd11537f2591b03940b7c0859d87f
Pascal Brouwers <pascal@h-o.nl>
2017-07-12 17:06:17 +0200

    [PZL-97] saved address custom attribute is object with key 'value'. New address has custom attribute as a string.

--- src/view/frontend/web/js/action/set-shipping-information-mixin.js
+++ src/view/frontend/web/js/action/set-shipping-information-mixin.js
@@ -23,10 +23,14 @@ define([
 
             shippingAddress['city'] = shippingAddress.city;
             if (shippingAddress.customAttributes['house_number']) {
-                shippingAddress['street'][0] = shippingAddress.customAttributes['street_name'].value;
-                shippingAddress['street'][1] = shippingAddress.customAttributes['house_number'].value;
+                var streetName = shippingAddress.customAttributes['street_name'].value ? shippingAddress.customAttributes['street_name'].value : shippingAddress.customAttributes['street_name'];
+                var houseNumber = shippingAddress.customAttributes['house_number'].value ? shippingAddress.customAttributes['house_number'].value : shippingAddress.customAttributes['house_number'];
+
+                shippingAddress['street'][0] = streetName;
+                shippingAddress['street'][1] = houseNumber;
                 if (shippingAddress.customAttributes['house_number_addition']) {
-                    shippingAddress['street'][2] = shippingAddress.customAttributes['house_number_addition'].value;
+                    var addition = shippingAddress.customAttributes['house_number_addition'].value ? shippingAddress.customAttributes['house_number_addition'].value : shippingAddress.customAttributes['house_number_addition'];
+                    shippingAddress['street'][2] = addition;
                 }
             }
             // pass execution to original action ('Magento_Checkout/js/action/set-shipping-information')
