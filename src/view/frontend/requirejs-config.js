27f220e458ef52bbcd30de6b4ce31b26b7d09487
Robbert <trebb0r@users.noreply.github.com>
2017-10-12 10:11:50 +0200

    Update requirejs-config.js

--- src/view/frontend/requirejs-config.js
+++ src/view/frontend/requirejs-config.js
@@ -16,20 +16,22 @@ var config = {
             },
             'Magento_Checkout/js/view/shipping': {
                 'Paazl_Shipping/js/view/shipping-mixin': true
+            },
+            'Magento_Checkout/js/view/billing-address': {
+                'Paazl_Shipping/js/view/billing-address-mixin': true
+            }
+        },
+        map: {
+            '*': {
+                fileElement: 'Paazl_Shipping/file-element',
+                'Magento_Checkout/js/model/shipping-rate-processor/new-address':
+                    'Paazl_Shipping/js/model/shipping-rate-processor/new-address',
+                'Magento_Checkout/template/billing-address/details.html':
+                    'Paazl_Shipping/template/billing-address/details.html',
+                'Magento_Checkout/template/shipping-address/address-renderer/default.html':
+                    'Paazl_Shipping/template/shipping-address/address-renderer/default.html',
+                'Magento_Checkout/template/shipping-information/address-renderer/default.html':
+                    'Paazl_Shipping/template/shipping-information/address-renderer/default.html'
             }
         }
-    },
-    map: {
-        '*': {
-            fileElement: 'Paazl_Shipping/file-element',
-            'Magento_Checkout/js/model/shipping-rate-processor/new-address':
-                'Paazl_Shipping/js/model/shipping-rate-processor/new-address',
-            'Magento_Checkout/template/billing-address/details.html':
-                'Paazl_Shipping/template/billing-address/details.html',
-            'Magento_Checkout/template/shipping-address/address-renderer/default.html':
-                'Paazl_Shipping/template/shipping-address/address-renderer/default.html',
-            'Magento_Checkout/template/shipping-information/address-renderer/default.html':
-                'Paazl_Shipping/template/shipping-information/address-renderer/default.html'
-        }
-    }
-};
+    };
